596cb90125fd78a0b2d1923669cd5667155f7c51
Amna Janahi <amna.m.janahi@gmail.com>
2025-07-29 16:13:25 +0300

    Creating a country

--- routes/country.routes.js
+++ routes/country.routes.js
@@ -8,6 +8,15 @@ router.get("/new", (req,res) => {
 res.render("country/new.ejs")
 })
 
+router.post("/",async (req,res) => {
+    try{
+        const createdCountry = await Country.create(req.body)
+        res.redirect("/country/new")
+    }
+    catch(error){
+        console.log("There is an error",error)
+    }
+})
 
 
 
